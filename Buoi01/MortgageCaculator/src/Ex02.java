e12e5ddb3619e202b33ca772e074efe867ad61a7
TheCuccu <hoangvancuong123@gmail.com>
2020-09-13 23:48:44 +0700

    sua lai Ex02

--- Buoi01/MortgageCaculator/src/Ex02.java
+++ Buoi01/MortgageCaculator/src/Ex02.java
@@ -12,27 +12,27 @@ public class Ex02 {
 
 //    hàm kiểm tra tên con giáp. có thể dùng switch case hoặc if-else
     public static void checkZodiacName(int number){
-        if (number % 11 == 0){//phép chia lấy phần dư, nếu chi cho 11 có phần dư là 0 -> Tý
+        if (number % 12 == 0){//phép chia lấy phần dư, nếu chi cho 12 có phần dư là 0 -> Tý
             System.out.println("Tý");
-        } else if (number % 11 == 1){
+        } else if (number % 12 == 1){
             System.out.println("Sửu");
-        } else if (number % 11 == 2){
+        } else if (number % 12 == 2){
             System.out.println("Dần");
-        } else if (number % 11 == 3){
+        } else if (number % 12 == 3){
             System.out.println("Mão");
-        } else if (number % 11 == 4){
+        } else if (number % 12 == 4){
             System.out.println("Thìn");
-        } else if (number % 11 == 5){
+        } else if (number % 12 == 5){
             System.out.println("Tỵ");
-        } else if (number % 11 == 6){
+        } else if (number % 12 == 6){
             System.out.println("Ngọ");
-        } else if (number % 11 == 7){
+        } else if (number % 12 == 7){
             System.out.println("Mùi");
-        } else if (number % 11 == 8){
+        } else if (number % 12 == 8){
             System.out.println("Thân");
-        } else if (number % 11 == 9){
+        } else if (number % 12 == 9){
             System.out.println("Dậu");
-        } else if (number % 11 == 10){
+        } else if (number % 12 == 10){
             System.out.println("Tuất");
         } else {
             System.out.println("Hợi");
